79b8c6ba6d259d8a5911e8da5f50a9ffaee39da7
Leo <liaoyuan@gmail.com>
2020-11-09 10:07:32 -0800

    theme-mod/patch-theme.sh: Improve maintainability and style
    - Use double quotes for global constant variables to avoid having to
      wrap every line in a pair of single quotes
    - Use arrays instead of space-separated lists
    - Use null character to terminate file names in 'grep' output to avoid
      potential issues with paths containing spaces
    - Guard file name arguments with '--' to prevent any file name that
      starts with a dash from being interpreted as an option
    - Fix other issues found by ShellCheck

--- theme-mod/patch-theme.sh
+++ theme-mod/patch-theme.sh
@@ -13,28 +13,30 @@
 # the site root or any directory one level down the site root.
 
 # The name of the theme's Ruby gem
-THEME_GEM='minimal-mistakes-jekyll'
+THEME_GEM="minimal-mistakes-jekyll"
 
 # Patterns for contents of theme files that should be patched
-PATTERNS=\
-'site\.title|'\
-'include_cached|'\
-'site\.locale|'\
-'\"%B %-d, %Y\"'
+PATTERNS="site\.title\
+|include_cached\
+|site\.locale\
+|\"%B %-d, %Y\"\
+"
 
 # The path to additional patches for specific files
-PATCHES_PATH='theme-mod/patches'
+PATCHES_PATH="theme-mod/patches"
 
-# Space-separated list of additional theme files that should be patched, even
-# if they do not contain matches of any PATTERNS
-ADDITIONAL_FILES=\
-'_includes/sidebar.html'
+# Array of additional theme files that should be patched, even if they do not
+# contain matches of any PATTERNS
+ADDITIONAL_FILES=(
+    "_includes/sidebar.html"
+)
 
-# Space-separated list of directories in the site root that contain theme files
-DIRS_WITH_THEME_FILES=\
-'assets '\
-'_includes '\
-'_layouts'
+# Array of directories in the site root that contain theme files
+DIRS_WITH_THEME_FILES=(
+    "assets"
+    "_includes"
+    "_layouts"
+)
 
 # Allow executing this script from directories one level below the site root
 if [[ -f ../Gemfile ]]; then
@@ -53,21 +55,22 @@ THEME_PATH=$(bundle info --path "${THEME_GEM}")
 # The label that will be inserted to every patched theme file
 # This is used for differentiating patched theme files from the site's own
 # assets, include files and layout files
-THEME_FILE_LABEL='patched-theme-file'
+THEME_FILE_LABEL="patched-theme-file"
 
 # If there are any old theme files, remove them
-if (grep -Fqr "${THEME_FILE_LABEL}" ${DIRS_WITH_THEME_FILES}); then
+if (grep -Fqr -- "${THEME_FILE_LABEL}" "${DIRS_WITH_THEME_FILES[@]}"); then
     echo "Deleting old files..."
-    grep -Flr "${THEME_FILE_LABEL}" ${DIRS_WITH_THEME_FILES} | xargs rm
+    grep -FZlr -- "${THEME_FILE_LABEL}" "${DIRS_WITH_THEME_FILES[@]}" | \
+        xargs -0 rm --
 fi
 
 echo "Getting files need to be patched..."
-grep --exclude='*.md' -Elr "${PATTERNS}" "${THEME_PATH}" | \
-    xargs cp --parents -t .
+grep --exclude='*.md' -EZlr -- "${PATTERNS}" "${THEME_PATH}" | \
+    xargs -0 cp --parents -t . --
 # Copy additional files which do not contain matches of PATTERNS
-for extra in ${ADDITIONAL_FILES}; do
+for extra in "${ADDITIONAL_FILES[@]}"; do
     if [[ ! -f "${extra}" ]]; then
-        cp --parents -t . "${THEME_PATH}/${extra}"
+        cp --parents -t . -- "${THEME_PATH}/${extra}"
     fi
 done
 
@@ -75,26 +78,26 @@ done
 temp_dir=$(realpath --relative-to=. "./${THEME_PATH}")
 
 echo "Patching files..."
-find "${temp_dir}" -type f -print0 | xargs -0 sed -i \
-    's/site\.title/site\.data\.l10n\.title/g'
-find "${temp_dir}" -type f -print0 | xargs -0 sed -i \
-    's/include_cached/include/g'
-find "${temp_dir}" -type f -print0 | xargs -0 sed -i \
-    's/site\.locale/site\.active_lang/g'
-find "${temp_dir}" -type f -print0 | xargs -0 sed -i \
-    's/"%B %-d, %Y"/site\.data\.l10n\.date_format/g'
+find -- "${temp_dir}" -type f -print0 | xargs -0 sed -i \
+    's/site\.title/site\.data\.l10n\.title/g' --
+find -- "${temp_dir}" -type f -print0 | xargs -0 sed -i \
+    's/include_cached/include/g' --
+find -- "${temp_dir}" -type f -print0 | xargs -0 sed -i \
+    's/site\.locale/site\.active_lang/g' --
+find -- "${temp_dir}" -type f -print0 | xargs -0 sed -i \
+    's/"%B %-d, %Y"/site\.data\.l10n\.date_format/g' --
 # Label the patched files
-find "${temp_dir}" -type f -name '*.js' -print0 | xargs -0 sed -i \
-    '$a\\n/* '${THEME_FILE_LABEL}' */'
-find "${temp_dir}" -type f ! -name '*.js' -print0 | xargs -0 sed -i \
-    '$a\\n{%- comment -%} '${THEME_FILE_LABEL}' {%- endcomment -%}'
+find -- "${temp_dir}" -type f -name '*.js' -print0 | xargs -0 sed -i \
+    "\$a\\\n/* ${THEME_FILE_LABEL} */" --
+find -- "${temp_dir}" -type f ! -name '*.js' -print0 | xargs -0 sed -i \
+    "\$a\\\n{%- comment -%} ${THEME_FILE_LABEL} {%- endcomment -%}" --
 
 echo "Installing patched files..."
-cp -r "${temp_dir:?}"/* .
+cp -r -- "${temp_dir:?}"/* .
 
 echo "Removing temporary files..."
-rm -r "${temp_dir:?}"/*
-rmdir -p "${temp_dir}"
+rm -r -- "${temp_dir:?}"/*
+rmdir -p -- "${temp_dir}"
 
 echo "Applying additional patches..."
 for patch in "${PATCHES_PATH}"/*; do
@@ -103,4 +106,4 @@ done
 
 # For minimal-mistakes-jekyll only
 echo "Copying the UI text file..."
-cp "${THEME_PATH}"/_data/ui-text.yml ./_data
+cp -- "${THEME_PATH}/_data/ui-text.yml" ./_data
